dc97a1ebe08b6373424f8b153fad4c4a4b08cfac
carlobonandriniunimi <carlo.bonandrini@studenti.unimi.it>
2023-03-10 19:06:25 +0100

    Changed dataset and added global visualization

--- src/eq_explore_data.py
+++ src/eq_explore_data.py
@@ -1,12 +1,30 @@
 from pathlib import Path
 import json
 
+import plotly.express as px
+
 # Read data as a string and convert to a Python object.
-path = Path('eq_data/eq_data_1_day_m1.geojson')
+path = Path('eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
-# Create a more readable version of the data file.
-path = Path('eq_data/readable_eq_data.geojson')
-readable_contents = json.dumps(all_eq_data, indent=4)
-path.write_text(readable_contents)
+# Examine all earthquakes in the dataset.
+all_eq_dicts = all_eq_data['features']
+
+# All mags
+mags, lons, lats, eq_titles = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    eq_titles.append(eq_dict['properties']['title'])
+
+title = 'Global Earthquakes'
+# * labels is a dictionary to specify ONLY the name of certain labels
+# * We set the color label to be Magnitude (this is the label on the right)
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title, color=mags,
+                     color_continuous_scale='Viridis',
+                     labels={'color': 'Magnitude'},
+                     projection='natural earth',
+                     hover_name=eq_titles)
+fig.show()
